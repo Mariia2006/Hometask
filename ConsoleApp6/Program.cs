985323742a02a87acc375d13ee42cb33f70ed248
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-19 10:55:08 +0300

    Update Program.cs

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -14,9 +14,9 @@ namespace Program
       if (a > 0 && a < 1000 && b > 0 && b < 1000 
   && c > 0 && c < 1000 && a+b > c && b+c > a && a+c > b)
       {
-        double m_a = 0.5 * 0.1 * (Math.Sqrt(2 * (Math.Pow(b, 2))) + (2 * (Math.Pow(c, 2))) - Math.Pow(c, 2));
-        double m_b = 0.5 * 0.1 * (Math.Sqrt(2 * (Math.Pow(a, 2))) + (2 * (Math.Pow(c, 2))) - Math.Pow(b, 2));
-        double m_c = 0.5 * 0.1 * (Math.Sqrt(2 * (Math.Pow(b, 2))) + (2 * (Math.Pow(a, 2))) - Math.Pow(c, 2));
+        double m_a = 0.5 * (Math.Sqrt(2 * (Math.Pow(b, 2))) + (2 * (Math.Pow(c, 2))) - Math.Pow(c, 2));
+        double m_b = 0.5 * (Math.Sqrt(2 * (Math.Pow(a, 2))) + (2 * (Math.Pow(c, 2))) - Math.Pow(b, 2));
+        double m_c = 0.5 * (Math.Sqrt(2 * (Math.Pow(b, 2))) + (2 * (Math.Pow(a, 2))) - Math.Pow(c, 2));
         Console.WriteLine(m_a);
         Console.WriteLine(m_b);
         Console.WriteLine(m_c);
