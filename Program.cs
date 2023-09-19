89be8ec372b63a48b00f3682f7e0b67cb28c1112
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-19 10:49:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿double a = ((0.3 * Math.Pow(5, 2) - 15) / (3.5 + Math.Pow(2, 2)));
-Console.WriteLine(a);
+using System;
+namespace Program {
+  class Program {
+    static void Main (string[] args) {
+      double a = ((0.3 * Math.Pow(5, 2) - 15) / (3.5 + Math.Pow(2, 2)));
+      Console.WriteLine(a);
+    }
+  }
+}
