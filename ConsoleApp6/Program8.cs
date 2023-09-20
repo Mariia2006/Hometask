216ec724fcf843bc0cb23ddc3945d6253587cfab
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-20 20:38:37 +0300

    Add files via upload

---
+++ ConsoleApp6/Program8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Double.Parse(Console.ReadLine());
+            double y = Double.Parse(Console.ReadLine());
+            double p = Double.Parse(Console.ReadLine());
+            if ( x <= 100 && x >= 1 && y >= 1 && y <= 100 && p >= 1 && p <= 1000) 
+            {
+                double result = (x * (p / (x + y))) * (y * (p / (x + y)));
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
