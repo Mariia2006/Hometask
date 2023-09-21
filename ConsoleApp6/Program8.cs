29891a4209f20c857a3d3c5960d60f63f0d58c97
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-21 19:12:31 +0300

    Update Program8.cs

--- ConsoleApp6/Program8.cs
+++ ConsoleApp6/Program8.cs
@@ -6,13 +6,15 @@ namespace ConsoleApp11
         static void Main(string[] args)
         {
             string[] data = Console.ReadLine().Trim().Split();
-            double x = Double.Parse(data[0]);
-            double y = Double.Parse(data[1]);
-            double p = Double.Parse(data[2]);
-            if ( x <= 100 && x >= 1 && y >= 1 && y <= 100 && p >= 1 && p <= 1000) 
+            double p = double.Parse(data[0]);
+            double x = double.Parse(data[1]);
+            double y = double.Parse(data[2]);
+            if (x <= 100 && x >= 1 && y >= 1 && y <= 100 && p >= 1 && p <= 1000)
             {
-                double result = (x * (p / (x + y))) * (y * (p / (x + y)));
-                Console.WriteLine(result);
+                double var = p / (x+y);
+                double a = x * var;
+                double b = y * var;
+                Console.WriteLine($"{a*b}");
             }
         }
     }
