b5edab4e1011612a0140b9478511df209a4b2221
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-24 18:52:52 +0300

    Add files via upload

---
+++ ConsoleApp6/Program11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int res = a + b;
+            string a1 = a.ToString();
+            string b1 = b.ToString();
+            string ress = res.ToString();
+            int ax = 10 - a1.Length;
+            Console.SetCursorPosition(ax, 3);
+            Console.WriteLine(a);
+            int bx = 10 - b1.Length;  
+            Console.SetCursorPosition(bx, 4);
+            Console.WriteLine(b);
+            Console.WriteLine("----------");
+            int resx = 10 - ress.Length;
+            Console.SetCursorPosition(resx, 6);
+            Console.WriteLine(ress);
+        }
+    }
+}
