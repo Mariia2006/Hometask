2e3a124711489e66716122cfa3f9e5177d372b68
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-24 19:32:28 +0300

    Add files via upload

---
+++ ConsoleApp6/Program12.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string team1 = Console.ReadLine();
+            string goal1 = Console.ReadLine();
+            string team2 = Console.ReadLine();
+            string goal2 = Console.ReadLine();
+            string colon = " : ";
+            int team1x = 13 - team1.Length;
+            Console.SetCursorPosition(team1x, 4);
+            Console.Write($"{team1}");
+            Console.SetCursorPosition(13, 4);
+            Console.Write(colon);
+            Console.Write($"{team2}");
+            Console.SetCursorPosition(12, 5);
+            Console.Write($"{goal1}");
+            Console.Write(colon);
+            Console.Write($"{goal2}");
+        }
+    }
+}
