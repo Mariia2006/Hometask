005d671936b779ddd855d188d8c280b166e139ac
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-24 13:33:44 +0300

    Update Program6.cs

--- ConsoleApp6/Program6.cs
+++ ConsoleApp6/Program6.cs
@@ -1,18 +1,21 @@
-﻿using System;
-namespace ConsoleApp9
+using System;
+namespace ConsoleApp13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double a = Double.Parse(Console.ReadLine());
-            double b = Double.Parse(Console.ReadLine());
-            double c = Double.Parse(Console.ReadLine());
-            double x = Double.Parse(Console.ReadLine());
-            if ( a >= 0.1 && a<= 10.0 && b >= 0.1 && b <= 10.0 && c >= 0.1 && c <= 10.0 && x >= 0.1 && x <= 1000.0)
+            string[] data = Console.ReadLine().Trim().Split();
+            double a = double.Parse(data[0]);
+            double b = double.Parse(data[1]);
+            double c = double.Parse(data[2]);
+            double x = double.Parse(data[3]);
+            if (a >= 0.1 && a <= 10.0 && b >= 0.1 && b <= 10.0 && c >= 0.1 && c <= 10.0 && x >= 0.1 && x <= 1000.0)
             {
-                double s = 2 * ((b * 10) * (a * 10) + (b * 10) * (c * 10) + (a * 10) * (c * 10));
-                Console.WriteLine(s*x/1000);   
+                double gen = 2 * (b * a * 100 + b * c * 100 + a * c * 100);
+                double s = gen * (x / 1000);
+                double d = Math.Ceiling(s);
+                Console.WriteLine(d);
             }
         }
     }
